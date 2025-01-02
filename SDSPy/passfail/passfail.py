5ffc9646427e01b16c6c9c2a6106f0d8ef395ffd
lheywang <leonard.heywang@gmail.com>
2025-01-02 20:39:45 +0100

    Renamed PASS FAIL module

---
+++ SDSPy/passfail/passfail.py
@@ -0,0 +1,24 @@
+# ============================================================================================================
+# Waveform.py
+# lheywang on 21/12/2024
+#
+# Base file for the waveform class
+#
+# ============================================================================================================
+from BaseOptionnalClass import SiglentBase
+
+
+class SiglentPassFail(SiglentBase):
+    """
+    pySDS [Files][SiglentPassFail] :    Class herited from SiglentBase.
+                                        Store all command related the control of automated tests
+
+        Attributes :
+            Herited from SiglentBase
+
+        Methods :
+            Private (0) :
+                None
+
+            Public (7):
+    """
