a385aeb9ebbbd7f62fe58a8f56a21d98022078ad
lheywang <leonard.heywang@gmail.com>
2025-01-04 11:21:19 +0100

    Edited doc generation scripts

--- doc-html.bat
+++ doc-html.bat
@@ -2,6 +2,16 @@
 echo ==========================================================
 echo Generating HTML documentation...
 
+if not exist "documentation" mkdir documentation
+cd documentation
+if not exist "html" mkdir html
+cd html
+rmdir /s /q SDSPy
+del index.html
+del SDSPy.html
+del search.js
+cd ../..
+
 pdoc --output-dir documentation/html/ .\SDSPy\
 
 echo Done !
--- doc-markdown.bat
+++ doc-markdown.bat
@@ -2,15 +2,40 @@
 echo ==========================================================
 echo Generating Markdown documentation...
 
+if not exist "documentation" mkdir documentation
+cd documentation
+if not exist "markdown" mkdir markdown
+cd ..
+
 cd .\SDSPy\
 for /D %%i in (*) do (
     echo|set /p="Generatic doc for %%i ... "
-    pydoc-markdown -p %%i > ..\documentation\markdown\%%i.md
+    if not exist "..\documentation\markdown\%%i" mkdir ..\documentation\markdown\%%i
+    rem pydoc-markdown -p %%i > ..\documentation\markdown\%%i\%%i.md
     echo Done !
 )
+
+cd ../documentation/markdown
+rmdir /s /q __pycache__ 
+rmdir /s /q trigger
+mkdir trigger
+
+cd ../../SDSPy
+cd trigger
+
+for %%f in (*.py) do (
+    echo|set /p="Generatic doc for %%f ... "
+    echo pydoc-markdown -p %%f > ..\..\documentation\markdown\trigger\%%f.md
+    echo Done !
+)
+
+cd ..
+
 echo|set /p="Generatic doc for SDSPy ... "
 pydoc-markdown -p pySDS > ..\documentation\markdown\SDSPy.md
 echo Done !
+
+
 
 
 echo Finished creating Markdown documentation !
